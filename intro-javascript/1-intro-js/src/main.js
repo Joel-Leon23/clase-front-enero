fa09bf0cbd4c61fb90073cdbd0c02c3bf8eefaee
Joel Leon <joeleoncs@outlook.com>
2024-04-23 20:29:33 -0600

    Video 2.
    Constantes y variables

---
+++ intro-javascript/1-intro-js/src/main.js
@@ -0,0 +1,14 @@
+let name = 'Leon'
+
+name = 'Alejandro'
+
+const lastName = 'Leon'
+
+const condition = true
+
+if (condition) {
+  const lastName = "Hyper"
+  console.log(lastName)
+}
+
+console.log('Hello world ' + name + ' ' + lastName);
