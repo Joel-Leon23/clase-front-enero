e35049eb0b9e3214586e8bc61d463dc4b3daa062
Joel Leon <joeleoncs@outlook.com>
2024-04-23 20:58:04 -0600

    Video 7
    Objetos relacionados y funciones

--- intro-javascript/1-intro-js/src/main.js
+++ intro-javascript/1-intro-js/src/main.js
@@ -2,10 +2,18 @@ const car = {
   id: 1,
   name: 'LaFerrari',
   date: new Date(),
-  brand: 'Ferrari',
-  price: 1000000
+  brand: {
+    id: 1,
+    name: 'Ferrari',
+    location: 'Europa',
+    age: 100
+  },
+  price: 1000000,
+  cheers: function () { return `Hello ${this.brand.name} ${this.name}` }
 }
 
 car.name = 'F40'
 car.price = 1500000
-console.log(car.name, car.price)
+const cheers = car.cheers()
+console.log(car)
+console.log(cheers)
