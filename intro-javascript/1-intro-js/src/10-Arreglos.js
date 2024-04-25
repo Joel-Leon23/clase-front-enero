6e3383bfafdcdd41c6b165bf92360bb6146b7adc
Joel Leon <joeleoncs@outlook.com>
2024-04-24 20:26:56 -0600

    Video 14
    Desestructurar objetos

--- intro-javascript/1-intro-js/src/10-Arreglos.js
+++ intro-javascript/1-intro-js/src/10-Arreglos.js
@@ -1,3 +1,27 @@
+const carros = ['Ferrari', 'Nissan GTR', 'Porsche', 'McLaren', 'Mercedes']
+
+carros.push('Mustang GT500', 'Challenger', 'Camaro')
+
+console.log(carros)
+
+const num = [10, 5, 6, 12, 22]
+
+num.push(332, 532, 535)
+
+console.log(num)
+
+carros.forEach(car => console.log(car))
+
+for (const car of carros) {
+  console.log(car)
+}
+
+for (let i = 0; i < carros.length; i++) {
+  const element = carros[i]
+  console.log('i' + carros[i])
+}
+
+
 const carros = ['Ferrari', 'Nissan', 'Porsche', 'McLaren', 'Mercedes']
 
 const carros2 = carros.concat('Mustang GT500', 'Challenger', 'Camaro')
--- intro-javascript/1-intro-js/src/main.js
+++ intro-javascript/1-intro-js/src/main.js
@@ -1,22 +1,13 @@
-const carros = ['Ferrari', 'Nissan GTR', 'Porsche', 'McLaren', 'Mercedes']
-
-carros.push('Mustang GT500', 'Challenger', 'Camaro')
-
-console.log(carros)
-
-const num = [10, 5, 6, 12, 22]
-
-num.push(332, 532, 535)
-
-console.log(num)
-
-carros.forEach(car => console.log(car))
-
-for (const car of carros) {
-  console.log(car)
+const carro = {
+  marca: 'Seat',
+  modelo: 'Fr',
+  year: 2018,
+  nombre: 'Leon'
 }
 
-for (let i = 0; i < carros.length; i++) {
-  const element = carros[i]
-  console.log('i' + carros[i])
-}
+// const marca = carro.marca
+
+const { marca, modelo, nombre } = carro
+
+console.log(marca)
+console.log(nombre)
