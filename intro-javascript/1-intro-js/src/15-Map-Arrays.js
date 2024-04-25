c59fd76d139eab4ea6c2088112e2aedced2266de
Joel Leon <joeleoncs@outlook.com>
2024-04-24 20:43:39 -0600

    Video 18
    find filter some

--- intro-javascript/1-intro-js/src/15-Map-Arrays.js
+++ intro-javascript/1-intro-js/src/15-Map-Arrays.js
@@ -1,6 +1,5 @@
 const carros = [
   {
-    id: 1,
     marca: 'Seat',
     modelo: 'Fr',
     year: 2018,
@@ -11,7 +10,6 @@ const carros = [
     }
   },
   {
-    id: 2,
     marca: 'Seat',
     modelo: 'Excellence',
     year: 2018,
@@ -22,7 +20,6 @@ const carros = [
     }
   },
   {
-    id: 3,
     marca: 'Seat',
     modelo: 'Cupra',
     year: 2018,
@@ -33,7 +30,6 @@ const carros = [
     }
   },
   {
-    id: 4,
     marca: 'Cupra',
     modelo: 'Vz',
     year: 2018,
@@ -44,7 +40,6 @@ const carros = [
     }
   },
   {
-    id: 5,
     marca: 'Cupra',
     modelo: 'PHEV',
     year: 2018,
@@ -63,15 +58,3 @@ const nombresCarros = carros.map(({ cliente }) => cliente.name)
 
 console.log(marcasCarros)
 console.log(nombresCarros)
-
-const carroPorNombre = carros.find(car => car.nombre === 'Formentor')
-
-console.log(carroPorNombre)
-
-const carroFilter = carros.filter(car => car.id !== 1)
-
-console.log(carroFilter)
-
-const carroSome = carros.some(car => car.cliente.name === 'Gustavo')
-
-console.log(carroSome)
