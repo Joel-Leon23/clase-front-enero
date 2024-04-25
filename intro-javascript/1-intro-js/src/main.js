8850cbf5a8dc0f333ff2ee9ad08b69399f02b062
Joel Leon <joeleoncs@outlook.com>
2024-04-24 20:38:12 -0600

    Video 17
    Funcion map en arreglos

--- intro-javascript/1-intro-js/src/main.js
+++ intro-javascript/1-intro-js/src/main.js
@@ -1,5 +1,60 @@
-const alumnos = ['monse', 'mau', 'gustavo', 'jose', 'barbara', 'juan']
+const carros = [
+  {
+    marca: 'Seat',
+    modelo: 'Fr',
+    year: 2018,
+    nombre: 'Leon',
+    cliente: {
+      name: 'Gustavo',
+      apellido: 'Rodriguez'
+    }
+  },
+  {
+    marca: 'Seat',
+    modelo: 'Excellence',
+    year: 2018,
+    nombre: 'Arona',
+    cliente: {
+      name: 'Gustavo',
+      apellido: 'Rodriguez'
+    }
+  },
+  {
+    marca: 'Seat',
+    modelo: 'Cupra',
+    year: 2018,
+    nombre: 'Ateca',
+    cliente: {
+      name: 'Gustavo',
+      apellido: 'Rodriguez'
+    }
+  },
+  {
+    marca: 'Cupra',
+    modelo: 'Vz',
+    year: 2018,
+    nombre: 'Formentor',
+    cliente: {
+      name: 'Gustavo',
+      apellido: 'Rodriguez'
+    }
+  },
+  {
+    marca: 'Cupra',
+    modelo: 'PHEV',
+    year: 2018,
+    nombre: 'Formentor',
+    cliente: {
+      name: 'Gustavo',
+      apellido: 'Rodriguez'
+    }
+  }
+]
 
-const [nom1, nom2, nom3, ...nombres] = alumnos
+console.log(carros)
 
-console.log(nom1, nom2, nom3, ...nombres)
+const marcasCarros = carros.map(({ marca }) => marca)
+const nombresCarros = carros.map(({ cliente }) => cliente.name)
+
+console.log(marcasCarros)
+console.log(nombresCarros)
